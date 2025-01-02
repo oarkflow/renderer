76db6f899c75f608451eba78a48643222242e019
Sunil Sapkota <suneelsapkota9@gmail.com>
2025-01-02 22:24:29 +0545

    add WebBuilder component and update dependencies; replace App with WebBuilder in main entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import WebBuilder from "./web/WebBuilder.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WebBuilder />
   </React.StrictMode>
 );
